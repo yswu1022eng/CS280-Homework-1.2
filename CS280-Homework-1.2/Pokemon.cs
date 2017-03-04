b558e74bf73d91f06baab148300f0c13ed903aaa
yswu1022eng <yswu1022eng@gmail.com>
2017-03-05 00:33:20 +0800

    新增Pokemon的類別(包含編號,名字等)

---
+++ CS280-Homework-1.2/Pokemon.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS280_Homework_1._2
+{
+    internal class Pokemon
+    {
+        public int Numero;
+        public string Name;
+        public float Height;
+        public float Weight;
+        public string Abilities;
+        public string Category;
+        public string Gender;
+    }
+}
---
+++ CS280-Homework-1.2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS280_Homework_1._2
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Pokemon pokemon = new Pokemon();
+            pokemon.Numero = 4;
+            pokemon.Name = "Charmander";
+            pokemon.Height = 2.0f;
+            pokemon.Weight = 12.7f;
+            pokemon.Category = "Lizard";
+            pokemon.Abilities = "Blaze";
+            pokemon.Gender = "M";
+            Console.WriteLine("編號:{0}, 名字:{1}, 身高:{2}呎, 體重:{3}磅, 特性:{4}, 性別:{5}", pokemon.Numero, pokemon.Name, pokemon.Height, pokemon.Weight, pokemon.Category, pokemon.Abilities, pokemon.Gender);
+        }
+    }
+}
